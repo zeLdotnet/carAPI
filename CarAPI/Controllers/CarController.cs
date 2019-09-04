66a4cb6058611ba80110fb0df6a8cd30baa183d4
zeL <3DenzeL@protonmail.com>
2019-09-04 17:44:14 -0400

    first commit to carAPI

---
+++ CarAPI/Controllers/CarController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace CarAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly CarDbContext _context;
+
+        public CarController(CarDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public async Task<ActionResult<IEnumerable<Car>>> GetCars()
+        {
+            var carList = await _context.Car.ToListAsync();
+            return carList;
+        }
+
+
+
+        // make method API method to search a car by make
+        [HttpGet("{Make}")]
+        public ActionResult<List<Car>> GetCarByMake(string make)
+        {
+            var found = _context.Car.Where(c => c.Make == make).ToList();
+            if (found == null)
+            {
+                return NotFound();
+            }
+            return found;
+        }
+
+        //[HttpPost("{Make}")]
+        //public async Task<ActionResult<Car>> PutMake(string make, Car car)
+        //{
+        //    var found = await _context.Car.FindAsync(make);
+        //    if (found.Make != make)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    _context.Entry(car).State = EntityState.Modified;
+        //    await _context.SaveChangesAsync();
+        //    return NoContent();
+        //}
+    }
+}
---
+++ CarAPI/Models/CarDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CarAPI.Models
+{
+    public partial class CarDbContext : DbContext
+    {
+        public CarDbContext()
+        {
+        }
+
+        public CarDbContext(DbContextOptions<CarDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Car { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=CarDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.Property(e => e.Color).HasMaxLength(30);
+
+                entity.Property(e => e.MYear).HasColumnName("mYear");
+
+                entity.Property(e => e.Make).HasMaxLength(30);
+
+                entity.Property(e => e.Model).HasMaxLength(30);
+            });
+        }
+    }
+}
